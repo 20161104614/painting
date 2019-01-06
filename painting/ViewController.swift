156b9250b89107fa3dc07bfc899f7263aa025eb5
20161104614 <964829629@qq.com>
2019-01-06 13:34:15 +0800

    Initial Commit

---
+++ painting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  painting
+//
+//  Created by 彭皓 on 2019/1/6.
+//  Copyright © 2019年 彭皓. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
